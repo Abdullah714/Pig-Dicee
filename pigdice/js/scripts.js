5819656ab6f7a5e05c5801d2e40876db15abfbcc
Mohamed Abdullahi <shalnark714@gmail.com>
2018-07-22 20:17:40 +0300

    comms

--- pigdice/js/scripts.js
+++ pigdice/js/scripts.js
@@ -49,7 +49,7 @@ Player.prototype.winnerCheck = function () {
 }
 
 Player.prototype.newGame = function () {
-  //debugger;
+  //debugger:
   this.roll = 0;
   this.tempscore = 0;
   this.totalscore = 0;
